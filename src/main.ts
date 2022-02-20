6402f3ac893a07a45971d37651c587bb32fa74c2
etherio <a>
2022-02-20 17:22:04 +0630

    force to run workflow action

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import '@core/admin';
 import { app } from '@core/app';
 import { connect } from '@core/db';
-import '@core/admin';
 
 connect().then(() =>
   app.listen(5000, () => console.log('server is running on port:', 5000))
